1539369701564a9fa5b87f04c4f6837a6b6c5a49
Senko <sendowsxp@gmail.com>
2025-01-21 21:09:46 +1300

    mass remove

--- foxgames/assets/js/project-page.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $('.back-button').hover(function() {
-    $(this + 'span p').addClass("anim-typewriter");
-  }, function() {
-    $(this + 'span p').removeClass("anim-typewriter");
-  })
-});
